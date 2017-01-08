93a7f1eabe4a54b7f3f175a5cf4eba50f53238d8
PeterPyPan <PeterPyPan@users.noreply.github.com>
2017-01-08 20:46:54 +0100

    Refactored selector

--- css/selector.py
+++ css/selector.py
@@ -2,33 +2,45 @@ from .style import Style
 from .utils import make_indent
 
 
-def create_element_selector(element_tag, class_tag=None, style=None):
-    return ElementSelector(element_tag=element_tag, class_tag=class_tag, style=style)
-
-
-def create_class_selector(class_tag, style=None):
-    return ClassSelector(class_tag=class_tag, style=style)
-
-
-def create_id_selector(id_tag, style=None):
-    return IdSelector(id_tag=id_tag, style=style)
-
-
-def create_selector_group(selectors, style=None):
-    return SelectorGroup(selectors=selectors, style=style)
+def create_selector(selectors=None, style=None):
+    return Selector(selectors, style)
 
 
 class Selector(object):
-    def __init__(self, style=None):
+    def __init__(self, selectors=None, style=None):
+        self._selectors = []
+        if selectors is None:
+            selectors = []
+        if isinstance(selectors, str):
+            selectors = [selectors]
+        self.add_selectors(selectors)
+
         if style is None:
             style = Style()
         elif not isinstance(style, Style):
             style = Style(style_dict=style)
         self.style = style
 
+    def add_selector(self, selector):
+        selector = str(selector)
+        if ',' in selector:
+            raise ValueError('Group selectors must be added separately')
+        if '\n' in selector or '\r' in selector or '\t' in selector:
+            raise ValueError('Newline and tab characters are not allowed in selectors')
+        self._selectors.append(selector)
+
+    def add_selectors(self, selectors):
+        for selector in selectors:
+            self.add_selector(selector)
+
     @property
     def selector_string(self):
-        raise NotImplementedError('This should be implemented in subclasses')
+        if len(self._selectors) == 0:
+            return ''
+        sel_string = []
+        for sel in self._selectors:
+            sel_string.append(sel)
+        return ", ".join(sel_string)
 
     def get_string(self, indent=0, tab_settings=None):
         string_list = list()
@@ -42,47 +54,3 @@ class Selector(object):
 
     def __str__(self):
         return self.get_string()
-
-
-class ElementSelector(Selector):
-    def __init__(self, element_tag, class_tag=None, style=None):
-        super(ElementSelector, self).__init__(style=style)
-        self._element_tag = element_tag
-        self._class_tag = class_tag
-
-    @property
-    def selector_string(self):
-        if self._class_tag is None:
-            return self._element_tag
-        else:
-            return "%s.%s" % (self._element_tag, self._class_tag)
-
-
-class ClassSelector(Selector):
-    def __init__(self, class_tag, style=None):
-        super(ClassSelector, self).__init__(style=style)
-        self._class_tag = class_tag
-
-    @property
-    def selector_string(self):
-        return ".%s" % self._class_tag
-
-
-class IdSelector(Selector):
-    def __init__(self, id_tag, style=None):
-        super(IdSelector, self).__init__(style=style)
-        self._id_tag = id_tag
-
-    @property
-    def selector_string(self):
-        return "#%s" % self._id_tag
-
-
-class SelectorGroup(Selector):
-    def __init__(self, selectors, style=None):
-        super(SelectorGroup, self).__init__(style=style)
-        self._selectors = selectors
-
-    @property
-    def selector_string(self):
-        return ", ".join([x.selector_string for x in self._selectors])
